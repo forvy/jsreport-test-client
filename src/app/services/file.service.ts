f59fedda7881d030ec74224a1a02d6df40297ada
Theodore <kira0171@yahoo.com>
2019-07-19 12:00:36 +0700

    working auth

--- src/app/services/file.service.ts
+++ src/app/services/file.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import * as FileSaver from "file-saver";
 
 @Injectable()
@@ -8,8 +8,8 @@ export class FileService {
   constructor(private http: HttpClient) { }
   // host = 'http://localhost:8001'; // activating this and uncommenting which use host would enabled access to auth
   hosturl = 'http://localhost:5488/api/report';
-  // key = 'YWRtaW46cGFzc3dvcmQ=';
-  // header = { 'Content-Type': 'application/json', 'Authorization': 'Basic ' + this.key };
+  key = 'YWRtaW46cGFzc3dvcmQ='; // enable as necessary
+  header = { 'Content-Type': 'application/json', 'Authorization': 'Basic ' + this.key }; // enable as necessary
 
   saveAs(blob: Blob, fileName: string, type: string) {
     let file = new Blob([blob], { type: `application/${type}` });
@@ -20,14 +20,14 @@ export class FileService {
   }
 
   fetchFile(parameter) {
-    // return this.http.post(this.hosturl, parameter, { headers: this.header, responseType: 'blob' });
-    return this.http.post(this.hosturl, parameter, { responseType: 'blob' });
+    return this.http.post(this.hosturl, parameter, { headers: this.header, responseType: 'blob' });
+    // return this.http.post(this.hosturl, parameter, { responseType: 'blob' });
     // return this.http.get(this.host + '/test2?shortid=' + shortid + '&preview=true', { responseType: 'blob' });
   }
 
   showFileXls(parameter) {
-    // return this.http.post(this.hosturl, parameter, { headers: this.header, responseType: 'text' });
+    return this.http.post(this.hosturl, parameter, { headers: this.header, responseType: 'text' });
     // return this.http.get(this.host + '/test2?shortid=' + shortid + '&preview=true', { responseType: 'text' });
-    return this.http.post(this.hosturl, parameter, { responseType: 'text' });
+    // return this.http.post(this.hosturl, parameter, { responseType: 'text' });
   }
 }
